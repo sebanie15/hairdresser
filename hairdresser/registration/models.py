a4b8e796855921c7db06daddbcf03a2ef40ff6b4
unknown <weronika.maron@tlen.pl>
2021-03-07 10:33:34 +0100

    Fix models

--- hairdresser/registration/models.py
+++ hairdresser/registration/models.py
@@ -1,79 +1,66 @@
-from __future__ import annotations
-
 from django.db import models
 
+from django.utils.translation import gettext as _
 
 # Create your models here.
-
-
-class Employee(models.Model):
-    """
-    A class to represents employee model.
-    """
-    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=20, verbose_name='imię')
-    last_name = models.CharField(max_length=30, verbose_name='nazwisko')
-    email_address = models.EmailField(verbose_name='email')
-    phone_number = models.CharField(max_length=12, verbose_name='numer telefonu')
-    is_employed = models.BooleanField(default=True, verbose_name='zatrudniony')
-
-    class Meta:
-        ordering = ('first_name', 'last_name')
-        verbose_name = 'Pracownik'
-        verbose_name_plural = 'Pracownicy'
-        unique_together = ('user', 'email_address')
 
 
 class Salon(models.Model):
     """
     A class to represents salon model.
     """
-    name = models.CharField(max_length=50, verbose_name='nazwa')
-    address = models.CharField(max_length=50, verbose_name='adres')
-    phone_number = models.CharField(max_length=12, verbose_name='numer telefonu')
-    open_from = models.TimeField(verbose_name='otwarcie')
-    open_to = models.TimeField(verbose_name='zamknięcie')
-    employees = models.ManyToManyField(Employee)
+
+    name = models.CharField(max_length=50, verbose_name=_('Salon name'))
+    address = models.CharField(max_length=50, verbose_name=_('Address'))
+    phone_number = models.CharField(max_length=12, verbose_name=_('Phone number'))
+    open_from = models.TimeField(verbose_name=_('Open from'))
+    open_to = models.TimeField(verbose_name=_('Open to'))
+    active = models.BooleanField(default=True, verbose_name=_('Is active'))
+    employees = models.ManyToManyField('auth.User', verbose_name=_('Assigned employees'))
 
     def __str__(self):
         return f'{self.name}'
 
     class Meta:
-        ordering = ('name',)
-        verbose_name_plural = 'Salony'
+
+        verbose_name = _('Salon')
+        verbose_name_plural = _('Salons')
 
 
 class Service(models.Model):
     """
     A class to represents service model.
     """
-    name = models.CharField(max_length=50, verbose_name='nazwa usługi')
-    service_length = models.IntegerField(default=30, verbose_name='czas trwania [min]')
-    price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='cena')
+
+    name = models.CharField(max_length=50, verbose_name=_('Service name'))
+    time = models.DecimalField(max_digits=5, decimal_places=2, verbose_name=_('Executing time'))
+    price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name=_('Price'))
 
     def __str__(self):
         return f'{self.name}'
 
     class Meta:
-        ordering = ('name', 'service_length', 'price')
-        verbose_name = 'Usługa'
-        verbose_name_plural = 'Usługi'
+
+        verbose_name = _('Service')
+        verbose_name_plural = _('Services')
 
 
 class Visit(models.Model):
     """
     A class to represents visit model.
     """
-    employee = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    salon = models.ForeignKey(Salon, on_delete=models.CASCADE)
-    service = models.ForeignKey(Service, null=True, on_delete=models.SET_NULL)
-    start = models.DateTimeField()
-    stop = models.DateTimeField()
-    client_name = models.CharField(max_length=50, verbose_name='imię i nazwisko')
-    client_phone_number = models.CharField(max_length=9, verbose_name='numer kontaktowy')
-    discount = models.CharField(max_length=20, verbose_name='rabat')
-    price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='cena')
+    employee = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name=_('Employee'))
+    salon = models.ForeignKey(Salon, on_delete=models.CASCADE, verbose_name=_('_Salon'))
+    service = models.ForeignKey(Service, null=True, on_delete=models.SET_NULL, verbose_name=_('Service'))
+    start = models.DateTimeField(verbose_name=_('From'))
+    stop = models.DateTimeField(verbose_name=_('To'))
+    client_name = models.CharField(max_length=50, verbose_name=_('Client name'))
+    client_phone_number = models.CharField(max_length=12, verbose_name=_("Client's phone number"))
+    finished = models.BooleanField(default=False, verbose_name=_('Is finished'))
+
+    discount = models.DecimalField(max_length=5, decimal_places=2, blank=True, verbose_name=_('Discount'))
+    price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name=_('Price'))
 
     class Meta:
-        verbose_name = 'Wizyta'
-        verbose_name_plural = 'Wizyty'
+        verbose_name = _('Visit')
+        verbose_name_plural = _('Visits')

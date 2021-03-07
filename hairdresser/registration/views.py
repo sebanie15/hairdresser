4857633a3050425cd408705f2071e9d8f68d82e5
unknown <weronika.maron@tlen.pl>
2021-03-07 16:30:05 +0100

    Allow users to browse salon list

--- hairdresser/registration/views.py
+++ hairdresser/registration/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 from .models import Salon
 # Create your views here.
 
 
 def hello(request):
-
     return render(request=request, template_name="registration/base.html", context={})
 
 
@@ -16,5 +18,7 @@ def salon_list(request):
 
 
 def salon_detail(request, salon_id):
+    employees = User.objects.filter(salon__id=salon_id)
     salon = get_object_or_404(Salon, id=salon_id)
-    return render(request=request, template_name='registration/salon_detail.html', context={'salon': salon})
+    return render(request=request, template_name='registration/salon_detail.html',
+                  context={'salon': salon, 'employees': employees})
